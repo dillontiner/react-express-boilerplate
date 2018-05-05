308f212ae659b6dd5b4ff3da28833a461a9c8b66
Dillon Tiner <dillontiner@users.noreply.github.com>
2018-05-05 14:48:37 -0600

    working version

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    response: ''
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
   render() {
     return (
       <div className="App">
@@ -30,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          { this.state.response } 
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const PORT = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+const app = express();
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
 });
 
-app.listen(5000, () => console.log('Example app listening on port 5000!'))
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+});
